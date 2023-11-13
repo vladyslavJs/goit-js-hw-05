62a92df8b0ed1af65ebcaa86fd7be093290d344f
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-11-13 21:56:09 +0100

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -5,12 +5,6 @@
 // другий параметр friendName — ім'я друга для пошуку.
 // Функція має повертати масив усіх користувачів із масиву users, у яких є друг з іменем friendName. Друзі кожного користувача зберігаються у властивості friends. Якщо користувачів, у яких є такий других немає, то функція має повернути порожній масив.
 
-const getUsersWithFriend = (users, friendName) => {
-
-};
-
-
-
 const allUsers = [
   {
     name: "Moore Hensley",
@@ -41,6 +35,8 @@ const allUsers = [
     friends: ["Goldie Gentry", "Briana Decker"]
   }
 ];
+
+const getUsersWithFriend = (users, friendName) => users.filter((user) => user.friends.includes(friendName));
 
 console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
 // [
