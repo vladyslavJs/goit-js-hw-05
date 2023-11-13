a1c91ec1170d068f9f30f23d08071870ae222723
vladyslavJs <vladyslavmelnyk15@gmail.com>
2023-11-13 19:48:45 +0100

    create_task_1_done

--- js/task-1.js
+++ js/task-1.js
@@ -1,35 +1,47 @@
-// Задача 1. Пакування товарів
+// Задача 1. Імена користувачів
+// Напиши стрілочну функцію getUserNames(users), яка прийматиме один параметр users — масив об'єктів користувачів. Функція має повертати масив імен усіх користувачів (властивість name) із масиву users.
 
-// Напиши функцію isEnoughCapacity(products, containerSize), яка обчислює, чи помістяться всі товари в контейнер при пакуванні.
-// Функція оголошує два параметри:
-// products — об'єкт, у якому ключі містять назви товарів, а їхні значення — кількість цих товарів. Наприклад, { apples: 2, grapes: 4 }.
-// containerSize — число, максимальна кількість одиниць товарів, яку в себе може вмістити контейнер.
-// Функція має повернути результат перевірки, чи помістяться всі товари в контейнер. Тобто порахувати загальну кількість товарів в об'єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
-
-function isEnoughCapacity(products, containerSize) {
-  let totalCapacity = 0;
-  const valueOfProd = Object.values(products);
-  
-  for (const value of valueOfProd) {
-    totalCapacity += value;
-  }
-  return totalCapacity <= containerSize ? true : false;
-}
-
-// Console 
+const getUserNames = (users) => {
+  const userName = users.map(user => user.name);
+  return userName;
+};
 
 console.log(
-  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-); // true
-
-console.log(
-  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-); // false
-
-console.log(
-  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
-); // true
-
-console.log(
-  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
-); // false
+  getUserNames([
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    balance: 2764
+  },
+])
+); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
